7455fc35c88294527fdc887cc14c28d059162c40
Dani1410 <wolfchis0@gmail.com>
2024-03-13 23:08:10 -0600

    V1.5 ColorCraft Agregamos Splash

--- app/src/main/java/com/example/main/MainActivity.java
+++ app/src/main/java/com/example/main/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.main;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
